15151ab9686e10b66daff0215ef00d5f8b82090a
Daians01 <duyng24062004@gmail.com>
2025-11-16 10:34:23 +0700

    Add files via upload

---
+++ QLy_Cinema-main/src/gui/MainSuatPhim.java
@@ -0,0 +1,16 @@
+package gui;
+
+import javax.swing.*;
+
+public class MainSuatPhim {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Quản Lý Suất Chiếu");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(600, 400);
+            frame.setLocationRelativeTo(null);
+            frame.setContentPane(new SuatChieu_GUI());
+            frame.setVisible(true);
+        });
+    }
+}
